aacc46cae4ee073b5d575fa7ae37313ef04d2e0a
Marquinhus Goncalves <eu@marquinhusgoncalves.com>
2020-02-03 20:23:21 -0300

    chore: add event in the elements

--- src/ui/ContactForm.java
+++ src/ui/ContactForm.java
@@ -1,6 +1,9 @@
 package ui;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ContactForm extends JFrame {
     private JPanel rootPanel;
@@ -13,5 +16,29 @@ public class ContactForm extends JFrame {
         setContentPane(rootPanel);
         setSize(500, 250);
         setVisible(true);
+
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        setLocation(dim.width / 2 - getSize().width / 2, dim.height / 2 - getSize().height / 2);
+
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        setListeners();
+    }
+
+    private void setListeners() {
+        buttonSave.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        buttonCancel.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new MainForm();
+                dispose();
+            }
+        });
     }
 }
--- src/ui/MainForm.java
+++ src/ui/MainForm.java
@@ -2,11 +2,13 @@ package ui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainForm extends JFrame{
     private JPanel rootPanel;
     private JButton buttonNewContact;
-    private JButton removeContact;
+    private JButton buttonRemove;
     private JTable tableContacts;
 
     public MainForm() {
@@ -18,5 +20,24 @@ public class MainForm extends JFrame{
         setLocation(dim.width / 2 - getSize().width / 2, dim.height / 2 - getSize().height / 2);
 
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        setListeners();
+    }
+
+    private void setListeners() {
+        buttonNewContact.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new ContactForm();
+                dispose();
+            }
+        });
+
+        buttonRemove.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
     }
 }
